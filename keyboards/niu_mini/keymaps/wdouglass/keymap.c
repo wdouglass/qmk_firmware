e8e5699d820fbf7d815694111d2216b091b0dba6
Woodrow Douglass <wdouglass@carnegierobotics.com>
2019-09-04 15:30:49 -0400

    sticky keys, disable swap.

--- keyboards/niu_mini/keymaps/wdouglass/keymap.c
+++ keyboards/niu_mini/keymaps/wdouglass/keymap.c
@@ -48,8 +48,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_QWERTY] = LAYOUT_planck_mit(
     KC_TAB,          KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     LCTL_T(KC_ESC),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-    KC_LSFT,         KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
-    MOUSE,           KC_LCTL, KC_LGUI, KC_LALT, LOWER,   SH_T(KC_SPC),    RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    OSM(MOD_LSFT),   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
+    MOUSE,           OSM(MOD_LCTL), OSM(MOD_LGUI), OSM(MOD_LALT), LOWER,   KC_SPC,    RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
     ),
 
 /* Lower
