0abcd56f606ef252f80921ac401034c580066808
JX <572673807@qq.com>
2021-08-30 00:05:02 +0800

    revert zhou65

--- keyboards/yandrstudio/zhou65/f401/rules.mk
+++ keyboards/yandrstudio/zhou65/f401/rules.mk
@@ -9,3 +9,9 @@ BOOTLOADER = stm32-dfu
 # ENCODER_ENABLE = yes
 # # project specific file
 CUSTOM_MATRIX = lite    # for using the A9 pin as matrix io
+QUANTUM_SRC += matrix.c
+
+EEPROM_DRIVER = custom
+SRC += eep/eeprom_stm32.c
+SRC += eep/flash_stm32.c
+OPT_DEFS += -DEEPROM_EMU_STM32F401xC
