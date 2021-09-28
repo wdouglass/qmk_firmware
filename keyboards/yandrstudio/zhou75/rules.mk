806498ff8cc2bbfc8c10b28c6c3481f11ab63af2
JX <572673807@qq.com>
2021-09-28 18:25:01 +0800

    fix win7

--- keyboards/yandrstudio/zhou75/rules.mk
+++ keyboards/yandrstudio/zhou75/rules.mk
@@ -3,7 +3,7 @@
 #
 BOOTMAGIC_ENABLE = lite     # Virtual DIP switch configuration
 KEYBOARD_SHARED_EP = no    # Free up some extra endpoints - needed if console+mouse+extra
-MOUSEKEY_ENABLE = yes       # Mouse keys
+MOUSEKEY_ENABLE = no       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
