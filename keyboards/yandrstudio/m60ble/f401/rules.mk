92ad1181219da3d82833aaa0117a5eadbd77dc0a
JX <572673807@qq.com>
2021-10-19 21:04:42 +0800

    fix m60 eeprom

--- keyboards/yandrstudio/m60ble/f401/rules.mk
+++ keyboards/yandrstudio/m60ble/f401/rules.mk
@@ -4,7 +4,7 @@ MCU = STM32F401
 # Address of the bootloader in system memory
 STM32_BOOTLOADER_ADDRESS = 0x1FFF0000
 # Bootloader selection
-BOOTLOADER = stm32-dfu
+BOOTLOADER = tinyuf2
 
 # # project specific file
 CUSTOM_MATRIX = lite    # for using the A9 pin as matrix io
