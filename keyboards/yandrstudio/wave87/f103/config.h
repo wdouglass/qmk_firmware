9fe56ebb1c118747a74636a93f8dbdbb4c80ab86
JX <572673807@qq.com>
2021-12-22 17:08:15 +0800

    修复引脚定义

--- keyboards/yandrstudio/wave87/f103/config.h
+++ keyboards/yandrstudio/wave87/f103/config.h
@@ -23,7 +23,7 @@
 
 //                        0    1    2    3     4    5    6    7    8    9    10    11   12  13   14   15   16   17
 #define MATRIX_COL_PINS { B8,  B9,  C13, C14,  C15, A0,  A3,  A4,  A5,  A6,  B0,   B1,  A1, A2,  B14, B13, B12 }
-#define MATRIX_ROW_PINS { B15, B3,  B4,  B5,   B6,  B7}
+#define MATRIX_ROW_PINS { A15, B3,  B4,  B5,   B6,  B7}
 
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
