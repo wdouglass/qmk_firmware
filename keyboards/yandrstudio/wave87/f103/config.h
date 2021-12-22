ab527dd53115b87e8ffc78fba73022ddb05d6b65
JX <572673807@qq.com>
2021-12-22 22:26:31 +0800

    更新wave87

--- keyboards/yandrstudio/wave87/f103/config.h
+++ keyboards/yandrstudio/wave87/f103/config.h
@@ -37,11 +37,11 @@
 #    define DRIVER_LED_TOTAL RGBLED_NUM
 
 
-#    define WS2812_PWM_DRIVER PWMD3  // default: PWMD2
-#    define WS2812_PWM_CHANNEL 2  // default: 2
-#    define WS2812_PWM_PAL_MODE 2  // Pin "alternate function", see the respective datasheet for the appropriate values for your MCU. default: 2
-#    define WS2812_DMA_STREAM STM32_DMA1_STREAM2  // DMA Stream for TIMx_UP, see the respective reference manual for the appropriate values for your MCU.
-#    define WS2812_DMA_CHANNEL 5  // DMA Channel for TIMx_UP, see the respective reference manual for the appropriate values for your MCU.
+#   define WS2812_PWM_DRIVER PWMD3  // default: PWMD2
+#   define WS2812_PWM_CHANNEL 2  // default: 2
+#   define WS2812_PWM_PAL_MODE 2
+#   define WS2812_DMA_STREAM STM32_DMA1_STREAM3
+#   define WS2812_DMA_CHANNEL 3
 
 
 #    define RGB_MATRIX_MAXIMUM_BRIGHTNESS 180
@@ -66,11 +66,11 @@
 #    define RGBLIGHT_ANIMATIONS
 #    define RGBLIGHT_SLEEP
 
-#define WS2812_PWM_DRIVER PWMD3  // default: PWMD2
-#define WS2812_PWM_CHANNEL 2  // default: 2
-#define WS2812_PWM_PAL_MODE 2  // Pin "alternate function", see the respective datasheet for the appropriate values for your MCU. default: 2
-#define WS2812_DMA_STREAM STM32_DMA1_STREAM2  // DMA Stream for TIMx_UP, see the respective reference manual for the appropriate values for your MCU.
-#define WS2812_DMA_CHANNEL 5  // DMA Channel for TIMx_UP, see the respective reference manual for the appropriate values for your MCU.
+#   define WS2812_PWM_DRIVER PWMD3  // default: PWMD2
+#   define WS2812_PWM_CHANNEL 2  // default: 2
+#   define WS2812_PWM_PAL_MODE 2
+#   define WS2812_DMA_STREAM STM32_DMA1_STREAM3
+#   define WS2812_DMA_CHANNEL 3
 
 // #    define WS2812_BYTE_ORDER WS2812_BYTE_ORDER_RGB
 
