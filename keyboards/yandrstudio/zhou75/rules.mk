ecdb87f0fe549f08b754d552c4884eb7c8774441
JX <572673807@qq.com>
2022-02-07 11:39:59 +0800

    Update rules.mk

--- keyboards/yandrstudio/zhou75/rules.mk
+++ keyboards/yandrstudio/zhou75/rules.mk
@@ -6,6 +6,6 @@ KEYBOARD_SHARED_EP = no    # Free up some extra endpoints - needed if console+mo
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
-COMMAND_ENABLE = yes         # Commands for debug and configuration
+COMMAND_ENABLE = no         # Commands for debug and configuration
 # if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 NKRO_ENABLE = yes           # USB Nkey Rollover
