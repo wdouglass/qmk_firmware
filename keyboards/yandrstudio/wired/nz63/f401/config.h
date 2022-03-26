3edc5c13b6c50c1831a4fcccefd80b2ee8a059d8
jiaxin96 <572673807@qq.com>
2022-03-26 22:18:38 +0800

    添加NZ63

--- keyboards/yandrstudio/wired/nz63/f401/config.h
+++ keyboards/yandrstudio/wired/nz63/f401/config.h
@@ -1,4 +1,4 @@
-/* Copyright 2020 QMK
+/* Copyright 2021 JasonRen(biu)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -13,15 +13,8 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 #pragma once
 
-#include_next <mcuconf.h>
-
-#undef STM32_PWM_USE_TIM3
-#define STM32_PWM_USE_TIM3 TRUE
-
-#undef STM32_PLLXTPRE
-#define STM32_PLLXTPRE STM32_PLLXTPRE_DIV2
+#include "config_common.h"
 
 
--- keyboards/yandrstudio/wired/nz63/readme.md
+++ keyboards/yandrstudio/wired/nz63/readme.md
@@ -11,6 +11,6 @@ Hardware Availability: https://github.com/Oh-My-Mechanical-Keyboard
 
 Make example for this keyboard (after setting up your build environment):
 
-    make yandrstudio/wired/nz63/f103:default
+    make yandrstudio/wired/nz63/f401:via
 
 See [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) then the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information.
