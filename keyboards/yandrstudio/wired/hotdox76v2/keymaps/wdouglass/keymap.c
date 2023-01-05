b61d1190771a479786dbf3f550c098f85ccd2271
Woodrow Douglass <wdouglass@carnegierobotics.com>
2023-01-05 08:33:57 -0500

    ugh no more toggles

--- keyboards/yandrstudio/wired/hotdox76v2/keymaps/wdouglass/keymap.c
+++ keyboards/yandrstudio/wired/hotdox76v2/keymaps/wdouglass/keymap.c
@@ -77,7 +77,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	LAYOUT(
         // left hand
         KC_EQL,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_LEFT,
-        KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   TG(1),
+        KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_TRNS,
         CTL_ESC,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_LPRN,
         LT(1,KC_GRV),   KC_QUOT,      LALT(KC_LSFT),  KC_LGUI, KC_LALT,
@@ -86,10 +86,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                  LOWER,KC_SPC,KC_END,
         // right hand
         KC_RGHT,     KC_6,   KC_7,  KC_8,   KC_9,   KC_0,             KC_MINS,
-        TG(2),       KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_BSPC,
+        KC_TRNS,     KC_Y,   KC_U,  KC_I,   KC_O,   KC_P,             KC_BSPC,
                      KC_H,   KC_J,  KC_K,   KC_L,   KC_SCLN,          KC_QUOT,
         KC_RPRN,     KC_N,   KC_M,  KC_COMM,KC_DOT, KC_SLSH,          KC_ENT,
-                             KC_LEFT, KC_DOWN,KC_UP,KC_RIGHT,          TT(1),
+                             KC_LEFT, KC_DOWN,KC_UP,KC_RIGHT,         KC_TRNS,
         KC_LALT,        CTL_T(KC_ESC),
         KC_PGUP,
         KC_PGDN,KC_SPC,RAISE
